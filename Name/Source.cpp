35c835d93dc0b49507aa961f63a8993d15f250c1
Ravisada Chamchoy <ravisada555@gmail.com>
2020-08-30 21:34:49 +0700

    Add project files.

---
+++ Name/Source.cpp
@@ -0,0 +1,113 @@
+#define CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int main()
+{
+	char name;
+	printf("Let me know your first character : ");
+	scanf_s("%c", &name);
+	if (name == 'a' || name == 'A')
+	{
+		printf("%c for Adorable one",name);
+	}
+	else if (name == 'b' || name == 'B')
+	{
+		printf("%c for Born to make history",name);
+	}
+	else if (name == 'c' || name == 'C')
+	{
+		printf("%c for Careing people around you", name);
+	}
+	else if (name == 'd' || name == 'D')
+	{
+		printf("%c for Did it best!", name);
+	}
+	else if (name == 'e' || name == 'E')
+	{
+		printf("%c for Excuse me, have we ever met before?", name);
+	}
+	else if (name == 'f' || name == 'F')
+	{
+		printf("%c for Far from every trouble", name);
+	}
+	else if (name == 'g' || name == 'G')
+	{
+		printf("%c for Good for you", name);
+	}
+	else if (name == 'h' || name == 'H')
+	{
+		printf("%c for High risk, high return;)", name);
+	}
+	else if (name == 'i' || name == 'I')
+	{
+		printf("%c for In the name of something special", name);
+	}
+	else if (name == 'j' || name == 'J')
+	{
+		printf("%c for Just a little bit messy", name);
+	}
+	else if (name == 'k' || name == 'K')
+	{
+		printf("%c for Kind of a nice one", name);
+	}
+	else if (name == 'l' || name == 'L')
+	{
+		printf("%c for Long distance doesn't bother us anyway", name);
+	}
+	else if (name == 'm' || name == 'M')
+	{
+		printf("%c for Marry me, please!", name);
+	}
+	else if (name == 'n' || name == 'N')
+	{
+		printf("%c for Nice to meet you today", name);
+	}
+	else if (name == 'o' || name == 'O')
+	{
+		printf("%c for Oh! What a lucky one.", name);
+	}
+	else if (name == 'p' || name == 'P')
+	{
+		printf("%c for Pure angel!", name);
+	}
+	else if (name == 'q' || name == 'Q')
+	{
+		printf("%c for Quit try with this right?", name);
+	}
+	else if (name == 'r' || name == 'R')
+	{
+		printf("%c for Right there! cuteness on your face", name);
+	}
+	else if (name == 's' || name == 'S')
+	{
+		printf("%c for Salary isn't enough for this month?", name);
+	}
+	else if (name == 't' || name == 'T')
+	{
+		printf("%c for Today is a good day", name);
+	}
+	else if (name == 'u' || name == 'U')
+	{
+		printf("%c for U R UNSTOPABLE NOW", name);
+	}
+	else if (name == 'v' || name == 'V')
+	{
+		printf("%c for V R the champian", name);
+	}
+	else if (name == 'w' || name == 'W')
+	{
+		printf("%c for We're gonna past this 2020 together right?", name);
+	}
+	else if (name == 'x' || name == 'X')
+	{
+		printf("%c for Excise more for your own shape", name);
+	}
+	else if (name == 'y' || name == 'Y')
+	{
+		printf("%c for Y are you look at me like that?", name);
+	}
+	else if (name == 'z' || name == 'Z')
+	{
+		printf("%c for Zzzz is da best", name);
+	}
+	return 0;
+}
